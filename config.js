b45727a525ba9886f2b4cc022237511ed780484c
jemagno1030 <jemagno1030@gmail.com>
2026-08-07 13:20:05 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,6 @@
 window.WMS_CONFIG = {
   SUPABASE_URL: 'https://sqpxgwhhfxojzblitnni.supabase.co',
   SUPABASE_ANON_KEY: 'sb_publishable_XJHgd8ODlbrrM_-9BJE0Kw_xache3hd',
-  APP_NAME: 'Warehouse Control System v1',
+  APP_NAME: 'IFTC Warehouse Locator System (JPM)',
   ALLOW_SIGNUP: true
 };
